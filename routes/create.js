3009185fc3eacaca47382ed02cf246c7c55ec6a1
vagrant <vagrant@precise32.(none)>
2014-02-07 16:50:59 +0000

    ready for class

--- routes/create.js
+++ routes/create.js
@@ -1,6 +1,6 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-   res.render('user_content/create_content', data);
+   res.render('create', data);
 }
 
--- routes/friends.js
+++ routes/friends.js
@@ -1,6 +1,6 @@
 var data = require('../data.json');
 
 exports.view = function(req, res){
-   res.render('social/friends', data);
+   res.render('friends', data);
 }
 
